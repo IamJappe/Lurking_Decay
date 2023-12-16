9d015992a75b6473cf7686405f048be58a746f86
IamJappe <deblock.jasper2007@gmail.com>
2023-12-16 16:45:44 +0100

    Player camera + movement
    Player camera works and movement. no gravity or jumping yet

---
+++ Assets/Main/Scripts/PlayerCamera.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public int sensitivity = 200;
+
+    float mouseX;
+    float mouseY;
+
+    private void Update()
+    {
+        mouseX += Input.GetAxisRaw("Mouse X") * sensitivity * Time.deltaTime;
+        mouseY -= Input.GetAxisRaw("Mouse Y") * sensitivity * Time.deltaTime;
+
+        transform.rotation = Quaternion.Euler(0f ,mouseX, 0f);
+        Camera.main.transform.localRotation = Quaternion.Euler(mouseY, 0f, 0f);
+    }
+}
---
+++ Assets/Main/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    int speed = 10;
+
+    float horizontal;
+    float vertical;
+    CharacterController controller;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        vertical = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        Vector3 move = transform.forward * vertical + transform.right * horizontal;
+        controller.Move(move);
+    }
+}
