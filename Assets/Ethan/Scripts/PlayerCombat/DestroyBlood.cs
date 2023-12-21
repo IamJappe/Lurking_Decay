c7888ab02a929446954ad34f3226035b49cf0a40
Ethan Amis <151955984+HeftyChomnker@users.noreply.github.com>
2023-12-21 17:43:34 -0500

    Finished the beginning of basic combat
    I added a dummy, I added blood, and i added punching.

---
+++ Assets/Ethan/Scripts/PlayerCombat/DestroyBlood.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyBlood : MonoBehaviour
+{
+
+    public GameObject blood;
+
+    void Start()
+    {
+        StartCoroutine(DestroyTheBlood());
+    }
+
+    IEnumerator DestroyTheBlood()
+    {
+        yield return new WaitForSeconds(0.7f);
+        Destroy(blood);
+    }
+}
---
+++ Assets/Ethan/Scripts/PlayerCombat/PlayerPunching.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using UnityEngine;
+
+public class PlayerPunching : MonoBehaviour
+{
+    public Animator handAnim;
+    public Transform punchPoint;
+    public float punchRange = 2f;
+    public GameObject bloodEffect;
+    private bool isPunching = false;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !isPunching)
+        {
+            StartCoroutine(Punch());
+        }
+
+    }
+
+    IEnumerator Punch()
+    {
+        isPunching = true;
+
+        handAnim.SetTrigger("Punch");
+
+        yield return new WaitForSeconds(0.01f);
+
+        RayCasts();
+
+       
+        yield return new WaitForSeconds(0.5f); 
+
+       
+        handAnim.ResetTrigger("Punch");
+        handAnim.SetTrigger("Bob");
+
+    
+        yield return new WaitForSeconds(0.1f);
+
+       
+        isPunching = false;
+    }
+
+    void RayCasts()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, punchRange))
+        {
+            if (hit.collider.CompareTag("Dummy"))
+            {
+                Debug.Log("Punched an enemy!");
+                Instantiate(bloodEffect, hit.point, Quaternion.FromToRotation(Vector3.up, hit.normal));
+            }
+            else
+            {
+                Debug.Log("Punched something else!");
+            }
+        }
+    }
+}
