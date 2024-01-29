11b66b0f1527aceab851e5c82be095d70ece910a
Ethan Amis <151955984+HeftyChomnker@users.noreply.github.com>
2024-01-29 16:49:20 -0500

    Crosshair bug

--- Assets/Ethan/Scripts/Interaction/RayCastInteractable.cs
+++ Assets/Ethan/Scripts/Interaction/RayCastInteractable.cs
@@ -1,26 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class RayCastInteractable : MonoBehaviour
 {
     public float interactionDistance = 4.5f;
-    public Image crossHair;
     public bool changeCrossHair = true;
 
+    private Image crossHair;
+
+    private void Start()
+    {
+        GameObject crossHairObject = GameObject.Find("Crosshair");
+
+        if (crossHairObject != null)
+        {
+            crossHair = crossHairObject.GetComponent<Image>();
+        }
+        else
+        {
+            Debug.LogError("Crosshair GameObject not found!");
+        }
+    }
 
     void Update()
-    {   
+    {
         int interactableLayerMask = 1 << LayerMask.NameToLayer("Interactable");
 
         if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, interactionDistance, interactableLayerMask))
-        {   
-            if(changeCrossHair == true)
+        {
+            if (changeCrossHair && crossHair != null)
                 crossHair.color = Color.red;
 
             InteractableTalking interactable = hit.collider.GetComponent<InteractableTalking>();
- 
+
             if (interactable != null && Input.GetMouseButtonDown(0))
             {
                 interactable.Interact();
@@ -28,8 +40,8 @@ public class RayCastInteractable : MonoBehaviour
         }
         else
         {
-            crossHair.color = Color.white;
+            if (crossHair != null)
+                crossHair.color = Color.white;
         }
     }
-
 }
